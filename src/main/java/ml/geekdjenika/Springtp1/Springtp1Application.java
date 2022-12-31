b65bfd3c0bc3678cd127d3d5d96d34003a4b20dc
Geek DJENIKA <djenikaa@gmail.com>
2022-12-31 07:21:06 +0000

    Importation du projet

---
+++ src/main/java/ml/geekdjenika/Springtp1/Springtp1Application.java
@@ -0,0 +1,13 @@
+package ml.geekdjenika.Springtp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springtp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springtp1Application.class, args);
+	}
+
+}
